222ec775cbf1181c20c0a9e9b8db0852bc2bcce3
dafeng.xdf <dafeng.xdf@taobao.com>
2014-07-21 14:16:30 +0800

    First Commit

--- Tomato/main.swift
+++ Tomato/main.swift
@@ -2,8 +2,8 @@
 //  main.swift
 //  Tomato
 //
-//  Created by xdf on 14-7-21.
-//  Copyright (c) 2014年 xdf. All rights reserved.
+//  Created by xdf on 7/21/14.
+//  Copyright (c) 2014 xdf. All rights reserved.
 //
 
 import Cocoa
