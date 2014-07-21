5e6040e37fe9018a54fd2120b80f8ddb20e744fb
dafeng.xdf <dafeng.xdf@taobao.com>
2014-07-21 11:46:31 +0800

    First commit

---
+++ Tomato/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tomato
+//
+//  Created by xdf on 14-7-21.
+//  Copyright (c) 2014年 xdf. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tomato/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Tomato
+//
+//  Created by xdf on 14-7-21.
+//  Copyright (c) 2014年 xdf. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
