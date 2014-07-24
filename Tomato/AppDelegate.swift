bea8a2f34e2a6ab43cc4c4bb6803f0cc3b798892
dafeng.xdf <dafeng.xdf@taobao.com>
2014-07-25 01:29:20 +0800

    Add function

--- Tomato/AppDelegate.swift
+++ Tomato/AppDelegate.swift
@@ -6,41 +6,49 @@
 //  Copyright (c) 2014 xdf. All rights reserved.
 //
 
-import Cocoa
+import Cocoa;
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet var window: NSWindow
+    @IBOutlet var myButton: NSButton
+    
     var label = NSText();
     var backgroundImage = NSImageView()
     var timeLabel = NSText();
     var run = false;
+    let DEFAULT_TIME = "5";
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         
         NSLog ("\nApp Start Launching ...");
         var title: NSString;
         title = "Pay more attention to do more.";
-        window.styleMask = 5;
+        window.styleMask = 2;
         window.alphaValue = 0.95;
         window.hasShadow = true;
-        window.setTitleWithRepresentedFilename(title);
-        window.center();
+        window.backgroundColor = NSColor.orangeColor();
         window.setFrame(NSRect(x: 0, y: 0, width: 300, height: 200), display: true);
+        window.center();
         label.alignment = .CenterTextAlignment;
-        label.frame = NSRect(x: 10, y: 10, width: 100, height: 20);
-        label.string = "test label";
+        label.backgroundColor = NSColor.clearColor();
+        label.textColor = NSColor.whiteColor();
+        label.font = NSFont(name:"Arial",size: 14);
+        label.frame = NSRect(x: 0, y: 170, width: self.window.frame.width, height: self.window.frame.height);
+        
+        label.string = title;
         window.contentView.addSubview(label);
         backgroundImage.frame = window.contentView.frame;
         backgroundImage.image = NSImage(named: "tomato");
         window.contentView.addSubview(backgroundImage);
-        timeLabel.string = "60"
-        timeLabel.frame = NSRect(x: 0, y: 0, width: 100, height: 40);
-        timeLabel.textColor = NSColor.redColor();
+        timeLabel.string = DEFAULT_TIME
+        timeLabel.frame = NSRect(x: 0, y: 65, width: self.window.frame.width, height: self.window.frame.height);
+        timeLabel.alignCenter(nil);
+        timeLabel.textColor = NSColor.whiteColor();
+        timeLabel.font = NSFont(name:"Arial",size: 30);
+        timeLabel.backgroundColor = NSColor.clearColor();
         window.contentView.addSubview(timeLabel);
         
         NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "countDown", userInfo: nil, repeats: true);
-
-        
     }
     
     func countDown () {
@@ -48,9 +56,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             return;
         }
         self.window.orderOut(nil);
-        if (timeLabel.string.toInt() == 50 ) {
-            timeLabel.string = "60";
-            self.window.orderFront(nil)
+        if (timeLabel.string.toInt() == 0 ) {
+            self.window.orderFront(nil);
+            self.myButton.title = "start";
+            self.window.level = Int(CGWindowLevelForKey(Int32(kCGScreenSaverWindowLevelKey))) ;
+            run = false;
         }
         else {
             let temp = timeLabel.string.toInt()! - 1;
@@ -59,7 +69,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     @IBAction func buttonTapped(sender: AnyObject) {
-        label.textColor = NSColor.redColor();
+        self.myButton.title = "stop";
         run = true;
     }
     
